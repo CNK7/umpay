9d9cd31254ae50eb9b66b916db0d69d2f7b3c0cb
KTM <128903581+CNK7@users.noreply.github.com>
2025-09-02 15:13:42 +0800

    Update wsgi.py

--- umpay/wsgi.py
+++ umpay/wsgi.py
@@ -1,13 +1,5 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-WSGI入口文件 - 专门为Vercel部署设计
-"""
-
+# Vercel WSGI Entry Point
 from app import app
 
-# Vercel需要的应用实例
-application = app
-
-if __name__ == "__main__":
-    application.run()
+# This is what Vercel will use
+app = app
